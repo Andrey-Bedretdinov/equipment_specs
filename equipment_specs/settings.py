e8b4556e363564d98bcb461f8e34fef74e7bc57a
andreybedretdinov <andrewbedretdinov0@gmail.com>
2025-06-03 13:54:55 +0300

    cors

--- equipment_specs/settings.py
+++ equipment_specs/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'apps.units',
     'apps.items',
     'drf_spectacular_sidecar',
+    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -64,6 +65,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -72,6 +74,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'equipment_specs.urls'
 
