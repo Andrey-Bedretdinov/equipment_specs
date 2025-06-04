cc237f5e65f284dfa1b2c6ad1064b2dc21a4b5d6
andreybedretdinov <andrewbedretdinov0@gmail.com>
2025-06-04 14:21:45 +0300

    urls swagger

--- equipment_specs/urls.py
+++ equipment_specs/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 from apps.catalog.urls import urlpatterns as catalog_urls
 from apps.projects.urls import urlpatterns as projects_urls
@@ -6,4 +7,9 @@ from apps.projects.urls import urlpatterns as projects_urls
 urlpatterns = [
     path('api/', include(catalog_urls)),
     path('api/', include(projects_urls)),
+
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
 ]
