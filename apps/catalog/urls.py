25b31e3fe1a9cb448b835a92236ca4394d431b0f
andreybedretdinov <andrewbedretdinov0@gmail.com>
2025-06-04 17:15:09 +0300

    фикс

--- apps/catalog/urls.py
+++ apps/catalog/urls.py
@@ -9,10 +9,8 @@ router.register(r'catalog/items', CatalogItemViewSet, basename='catalog-items')
 router.register(r'catalog/units', CatalogUnitViewSet, basename='catalog-units')
 router.register(r'catalog/kts', CatalogKTSViewSet, basename='catalog-kts')
 
-urlpatterns = [
-    path('catalog/items/', CatalogItemAPIView.as_view(), name='catalog-item-edit'),
-    path('catalog/units/', CatalogUnitAPIView.as_view(), name='catalog-unit-edit'),
-    path('catalog/kts/', CatalogKTSAPIView.as_view(), name='catalog-kts-edit'),
+urlpatterns = router.urls + [  # <-- Сначала роутер!
+    path('catalog/items/edit/', CatalogItemAPIView.as_view(), name='catalog-item-edit'),
+    path('catalog/units/edit/', CatalogUnitAPIView.as_view(), name='catalog-unit-edit'),
+    path('catalog/kts/edit/', CatalogKTSAPIView.as_view(), name='catalog-kts-edit'),
 ]
-
-urlpatterns += router.urls
