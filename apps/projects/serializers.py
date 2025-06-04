622c0bc3e862e621c1f13afdf75c67af30ba856f
andreybedretdinov <andrewbedretdinov0@gmail.com>
2025-06-04 14:59:48 +0300

    fix tree

--- apps/projects/serializers.py
+++ apps/projects/serializers.py
@@ -1,33 +1,120 @@
-from apps.catalog.serializers import CatalogItemSerializer, CatalogUnitSerializer, CatalogKTSSerializer
 from rest_framework import serializers
 
+from apps.catalog.models import CatalogUnitItem, CatalogKTSUnit, CatalogKTSItem
 from .models import Project, ProjectKTS, ProjectUnit, ProjectItem
 
 
-class ProjectItemSerializer(serializers.ModelSerializer):
-    item = CatalogItemSerializer(read_only=True)
-
-    class Meta:
-        model = ProjectItem
-        fields = ['id', 'item', 'quantity']
-
-
-class ProjectUnitSerializer(serializers.ModelSerializer):
-    unit = CatalogUnitSerializer(read_only=True)
-
-    class Meta:
-        model = ProjectUnit
-        fields = ['id', 'unit', 'quantity']
+# изделие
+class ItemSerializer(serializers.Serializer):
+    id = serializers.IntegerField(source='item.id')
+    name = serializers.CharField(source='item.name')
+    description = serializers.CharField(source='item.description')
+    supplier = serializers.CharField()
+    catalog_code = serializers.CharField()
+    price = serializers.DecimalField(max_digits=12, decimal_places=2)
+    currency = serializers.CharField()
+    manufactured = serializers.CharField()
+    delivery_type = serializers.CharField()
+    quantity = serializers.IntegerField()
 
 
-class ProjectKTSSerializer(serializers.ModelSerializer):
-    kts = CatalogKTSSerializer(read_only=True)
+# юнит с изделиями
+class UnitWithItemsSerializer(serializers.Serializer):
+    id = serializers.IntegerField(source='unit.id')
+    name = serializers.CharField(source='unit.name')
+    description = serializers.CharField(source='unit.description')
+    quantity = serializers.IntegerField()
+    items = serializers.SerializerMethodField()
 
-    class Meta:
-        model = ProjectKTS
-        fields = ['id', 'kts', 'quantity']
+    def get_items(self, obj):
+        unit_items = CatalogUnitItem.objects.filter(unit=obj.unit)
+        return [
+            {
+                "id": item.item.id,
+                "name": item.item.name,
+                "description": item.item.description,
+                "supplier": item.item.supplier,
+                "catalog_code": item.item.catalog_code,
+                "price": item.item.price,
+                "currency": item.item.currency,
+                "manufactured": item.item.manufactured,
+                "delivery_type": item.item.delivery_type,
+                "quantity": item.quantity
+            }
+            for item in unit_items
+        ]
 
 
+# ктс с юнитами и изделиями
+class KTSWithUnitsItemsSerializer(serializers.Serializer):
+    id = serializers.IntegerField(source='kts.id')  # <-- поправить доступ
+    name = serializers.CharField(source='kts.name')
+    description = serializers.CharField(source='kts.description')
+    quantity = serializers.IntegerField()
+    units = serializers.SerializerMethodField()
+    items = serializers.SerializerMethodField()
+
+    def get_units(self, obj):
+        kts_units = CatalogKTSUnit.objects.filter(kts=obj.kts)
+        return [
+            {
+                "id": unit.unit.id,
+                "name": unit.unit.name,
+                "description": unit.unit.description,
+                "quantity": unit.quantity,
+                "items": [
+                    {
+                        "id": item.item.id,
+                        "name": item.item.name,
+                        "description": item.item.description,
+                        "supplier": item.item.supplier,
+                        "catalog_code": item.item.catalog_code,
+                        "price": item.item.price,
+                        "currency": item.item.currency,
+                        "manufactured": item.item.manufactured,
+                        "delivery_type": item.item.delivery_type,
+                        "quantity": item.quantity
+                    }
+                    for item in CatalogUnitItem.objects.filter(unit=unit.unit)
+                ]
+            }
+            for unit in kts_units
+        ]
+
+    def get_items(self, obj):
+        kts_items = CatalogKTSItem.objects.filter(kts=obj.kts)
+        return [
+            {
+                "id": item.item.id,
+                "name": item.item.name,
+                "description": item.item.description,
+                "supplier": item.item.supplier,
+                "catalog_code": item.item.catalog_code,
+                "price": item.item.price,
+                "currency": item.item.currency,
+                "manufactured": item.item.manufactured,
+                "delivery_type": item.item.delivery_type,
+                "quantity": item.quantity
+            }
+            for item in kts_items
+        ]
+
+
+# изделие напрямую в проекте
+class ProjectItemSerializer(serializers.Serializer):
+    id = serializers.IntegerField(source='item.id')
+    name = serializers.CharField(source='item.name')
+    description = serializers.CharField(source='item.description')
+    supplier = serializers.CharField(source='item.supplier')
+    catalog_code = serializers.CharField(source='item.catalog_code')
+    price = serializers.DecimalField(source='item.price', max_digits=12, decimal_places=2)
+    currency = serializers.CharField(source='item.currency')
+    manufactured = serializers.CharField(source='item.manufactured')
+    delivery_type = serializers.CharField(source='item.delivery_type')
+    quantity = serializers.IntegerField()
+
+
+# основной проект
 class ProjectSerializer(serializers.ModelSerializer):
     kts_list = serializers.SerializerMethodField()
     units_list = serializers.SerializerMethodField()
@@ -39,11 +126,11 @@ class ProjectSerializer(serializers.ModelSerializer):
 
     def get_kts_list(self, obj):
         kts = ProjectKTS.objects.filter(project=obj)
-        return ProjectKTSSerializer(kts, many=True).data
+        return KTSWithUnitsItemsSerializer(kts, many=True).data
 
     def get_units_list(self, obj):
         units = ProjectUnit.objects.filter(project=obj)
-        return ProjectUnitSerializer(units, many=True).data
+        return UnitWithItemsSerializer(units, many=True).data
 
     def get_items_list(self, obj):
         items = ProjectItem.objects.filter(project=obj)
